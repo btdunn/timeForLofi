1533b73465ca9d8f4113fdd4d90fbd014bdaeb40
Ben Dunn <benthedunn@gmail.com>
2020-10-28 15:03:48 -0600

    react native experiments

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ImageBackground, Button, Text, View} from 'react-native';
 
 export default function App() {
+  
+
+  const [count, setCount] = useState(0)
+
+  const handlePress = () => {
+    setCount(count + 100)
+  }
+
+  const image = {uri: "https://danbooru.donmai.us/data/sample/sample-77c5eb102bd7cee0c369e17c313e1ca8.jpg"}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <ImageBackground source={image} style={styles.image}>
+      <View style={styles.container}>
+        <Text>welcome to hellllll{count} </Text>
+        <Button onPress={handlePress} title="hell button"></Button>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    height: null,
+    width: null,
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  }
 });
