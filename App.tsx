58e6675f47b8f83fb5ae7391e9669338ce61cce7
Ben Dunn <benthedunn@gmail.com>
2020-10-29 15:02:27 -0600

    added basic bg and timer

--- App.tsx
+++ App.tsx
@@ -1,35 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, ImageBackground, Button, Text, View} from 'react-native';
+import Timer from './components/Timer'
+
 
 export default function App() {
   
-
-  const [count, setCount] = useState(0)
-
-  const handlePress = () => {
-    setCount(count + 100)
-  }
-
-  const image = {uri: "https://danbooru.donmai.us/data/sample/sample-77c5eb102bd7cee0c369e17c313e1ca8.jpg"}
+  const image = {uri: "https://i.imgur.com/IAYgsSi.png"}
 
   return (
 
     <ImageBackground source={image} style={styles.image}>
-      <View style={styles.container}>
-        <Text>welcome to hellllll{count} </Text>
-        <Button onPress={handlePress} title="hell button"></Button>
+      <View>
+        <Timer />
       </View>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   image: {
     height: null,
     width: null,
---
+++ components/Timer.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Animated} from 'react-native';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+
+
+
+
+  const Timer = () => (
+    <CountdownCircleTimer
+      isPlaying
+      duration={10}
+      colors={[
+        ['#004777', 0.4],
+        ['#F7B801', 0.4],
+        ['#A30000', 0.2],
+      ]}
+    >
+      {({ remainingTime, animatedColor }) => (
+        <Animated.Text style={{ color: animatedColor }}>
+          {remainingTime}
+        </Animated.Text>
+      )}
+    </CountdownCircleTimer>
+  )
+
+export default Timer
