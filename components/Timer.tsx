f54e93e7a69ce84b7941c739085e277cfe168e3d
sydneygold <sydney.goldberg.com>
2020-10-30 10:53:57 -0600

    implemented working toggle button

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,35 +1,40 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Animated} from 'react-native';
+import { StyleSheet, Text, View, Animated, ViewComponent, Button} from 'react-native';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 
-// const [seconds, setSeconds] = useState(10)
-
-// const minuteSeconds = 60;
-
-
-// const getTimeSeconds = (time: number) => (minuteSeconds - time / 1000) | 0;
-// const getTimeMinutes = (time: number) => ((time) / minuteSeconds) | 0;
 
 
 
 
-  const Timer = () => (
-    <CountdownCircleTimer
-      isPlaying
-      duration={10}
-      colors={[
-        ['#004777', 0.4],
-        ['#F7B801', 0.4],
-        ['#A30000', 0.2],
-      ]}
-    >
-      {({ remainingTime, animatedColor }) => (
-        <Animated.Text style={{ color: animatedColor }}>
-          {remainingTime}
-        </Animated.Text>
-      )}
-    </CountdownCircleTimer>
-  )
+
+const Timer = () => {
+  const [playing, setPlaying] = useState(false)
+  
+    const togglePlaying = () => {
+      setPlaying(!playing)
+    }
+
+    return(
+    <View>
+      <CountdownCircleTimer
+        isPlaying={playing}
+        duration={10}
+        colors={[
+          ['#004777', 0.4],
+          ['#F7B801', 0.4],
+          ['#A30000', 0.2],
+        ]}
+      >
+        {({ remainingTime, animatedColor }) => (
+          <Animated.Text style={{ color: animatedColor }}>
+            {remainingTime}
+          </Animated.Text>
+        )}
+      </CountdownCircleTimer>
+      <Button onPress={togglePlaying} title="Toggle"></Button>
+    </View>
+    )
+}
 
   
 
